88ffdf43ef46e941ff3b0597ed5317efd98aa5c3
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-02-10 13:30:30 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "aws" {
-    	access_key = "${var.AWS_ACCESS_KEY}"
-	secret_key = "${var.AWS_SECRET_KEY}"
+#     	access_key = "${var.AWS_ACCESS_KEY_ID}"
+# 	secret_key = "${var.AWS_SECRET_KEY}"
     	region = "us-east-1"
 }
 
