9d3f9fe0f63febabaa75773ec51c22a48847dab2
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-02-24 10:45:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>ISM Team introduce for GitHub Actions</h1>
+        <h1>IMS Team introduce for GitHub Actions</h1>
       </header>
       <MainContent />
     </>
