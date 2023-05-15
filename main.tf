e7a3e86d267cfd3d260087888e3485cbf0a9ef79
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-15 19:07:06 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "aws_s3_bucket_public_access_block" "static" {
   bucket = aws_s3_bucket.static.id
 
   block_public_acls       = false
+  block_public_policy     = false
 }
 # resource "aws_s3_bucket_acl" "static" {
 #   bucket = aws_s3_bucket.static.id
