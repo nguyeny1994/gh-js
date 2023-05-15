e1f55daf0f0cc5da7bfde104a229b73f88d178ca
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-15 15:55:07 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
 #     	access_key = "${var.AWS_ACCESS_KEY_ID}"
 # 	    secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-    	region = "us-east-1"
+    region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "static" {
