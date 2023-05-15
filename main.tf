f766562bbf718c5848504f3caab7e79256a348d5
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-15 21:17:23 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,33 +30,33 @@ module "s3_bucket" {
     enabled = true
   }
 }
-resource "aws_s3_bucket_public_access_block" "static" {
-    bucket = aws_s3_bucket.static.bucket
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false
-}
+# resource "aws_s3_bucket_public_access_block" "static" {
+#     bucket = aws_s3_bucket.static.bucket
+#     block_public_acls = false
+#     block_public_policy = false
+#     ignore_public_acls = false
+#     restrict_public_buckets = false
+# }
 
-resource "aws_s3_bucket_ownership_controls" "static" {
-    bucket = aws_s3_bucket.static.bucket
-    rule {
-        object_ownership = "BucketOwnerPreferred"
-    }
-}
-resource "aws_s3_bucket_website_configuration" "static" {
-  bucket = aws_s3_bucket.static.bucket
+# resource "aws_s3_bucket_ownership_controls" "static" {
+#     bucket = aws_s3_bucket.static.bucket
+#     rule {
+#         object_ownership = "BucketOwnerPreferred"
+#     }
+# }
+# resource "aws_s3_bucket_website_configuration" "static" {
+#   bucket = aws_s3_bucket.static.bucket
 
-  index_document {
-    suffix = "index.html"
-  }
+#   index_document {
+#     suffix = "index.html"
+#   }
 
-  error_document {
-    key = "error.html"
-  }
-}
+#   error_document {
+#     key = "error.html"
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "static" {
-  bucket = aws_s3_bucket.static.id
-  policy = file("s3_static_policy.json")
-}
+# resource "aws_s3_bucket_policy" "static" {
+#   bucket = aws_s3_bucket.static.id
+#   policy = file("s3_static_policy.json")
+# }
