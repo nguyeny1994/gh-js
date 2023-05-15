f5f4ecf60be47562ea85a2193fb898ccba32ee3f
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-15 20:18:19 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,47 +18,49 @@ resource "aws_s3_bucket" "static" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_public_access_block" "static" {
+resource "aws_s3_bucket_policy" "static" {
     bucket = aws_s3_bucket.static.bucket
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false
+    policy = data.aws_iam_policy_document.static.json
 }
 
-resource "aws_s3_bucket_ownership_controls" "static" {
-    bucket = aws_s3_bucket.static.bucket
-    rule {
-        object_ownership = "BucketOwnerPreferred"
+data "aws_iam_policy_document" "static" {
+    statement {
+        sid = "PublicRead"
+        effect = "Allow"
+
+        principals {
+            type = "AWS"
+            identifiers = [ "*" ]
+        }
+
+        actions = [
+            "s3:GetObject",
+            "s3:GetObjectVersion"
+        ]
+
+        resources = [
+            "${aws_s3_bucket.static.arn}/*"
+        ]
     }
 }
 
-resource "aws_s3_bucket_acl" "static" {
-    depends_on = [
-        aws_s3_bucket_public_access_block.static,
-        aws_s3_bucket_ownership_controls.static,
-    ]
 
+resource "aws_s3_bucket_acl" "static" {
     bucket = aws_s3_bucket.static.bucket
+
+    # TODO This might be able to be `private` when CloudFront is up
     acl = "public-read"
 }
-
-resource "aws_s3_bucket_policy" "static_policy" {
-    bucket = aws_s3_bucket.static.bucket
-#     policy = data.aws_iam_policy_document.static_policy.json
-    policy = file("s3_static_policy.json")
-}
 resource "aws_s3_bucket_website_configuration" "static" {
-  bucket = aws_s3_bucket.static.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
+    bucket = aws_s3_bucket.static.bucket
+    index_document {
+        suffix = "index.html"
+    }
+    error_document {
+        key = "error.html"
+    }
 }
+
 # resource "aws_s3_bucket_public_access_block" "static" {
 #   bucket = aws_s3_bucket.static.id
 
