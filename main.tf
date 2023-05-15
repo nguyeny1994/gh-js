d6f5ddb191c25a8611c274b6341013a357b3a582
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-15 20:10:12 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,34 @@ resource "aws_s3_bucket" "static" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "static" {
+    bucket = aws_s3_bucket.static.bucket
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "static" {
+    bucket = aws_s3_bucket.static.bucket
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
+
+resource "aws_s3_bucket_acl" "static" {
+    depends_on = [
+        aws_s3_bucket_public_access_block.static,
+        aws_s3_bucket_ownership_controls.static,
+    ]
+
+    bucket = aws_s3_bucket.static.bucket
+    acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "static_policy" {
+    bucket = aws_s3_bucket.static.bucket
+    policy = data.aws_iam_policy_document.static_policy.json
 resource "aws_s3_bucket_website_configuration" "static" {
   bucket = aws_s3_bucket.static.bucket
 
@@ -29,14 +57,14 @@ resource "aws_s3_bucket_website_configuration" "static" {
     key = "error.html"
   }
 }
-resource "aws_s3_bucket_public_access_block" "static" {
-  bucket = aws_s3_bucket.static.id
+# resource "aws_s3_bucket_public_access_block" "static" {
+#   bucket = aws_s3_bucket.static.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+# }
 
-resource "aws_s3_bucket_policy" "static" {
-  bucket = aws_s3_bucket.static.id
-  policy = file("s3_static_policy.json")
-}
+# resource "aws_s3_bucket_policy" "static" {
+#   bucket = aws_s3_bucket.static.id
+#   policy = file("s3_static_policy.json")
+# }
