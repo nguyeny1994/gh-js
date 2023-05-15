e0e2962d24c30bf6146fd959d561eaad18ad0f70
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-15 18:38:03 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,28 +17,27 @@ resource "aws_s3_bucket" "static" {
   bucket        = "gh-js"
   force_destroy = true
 }
-
-resource "aws_s3_bucket_policy" "static" {
+resource "aws_s3_bucket_public_access_block" "static" {
   bucket = aws_s3_bucket.static.id
-  policy = file("s3_static_policy.json")
+
+  block_public_acls       = false
 }
+# resource "aws_s3_bucket_acl" "static" {
+#   bucket = aws_s3_bucket.static.id
+#   acl    = "public-read"
+# }
 
-resource "aws_s3_bucket_acl" "static" {
-  bucket = aws_s3_bucket.static.id
-  acl    = "public-read"
-}
+# resource "aws_s3_bucket_website_configuration" "static" {
+#   bucket = aws_s3_bucket.static.bucket
 
-resource "aws_s3_bucket_website_configuration" "static" {
-  bucket = aws_s3_bucket.static.bucket
+#   index_document {
+#     suffix = "index.html"
+#   }
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
+#   error_document {
+#     key = "error.html"
+#   }
+# }
 
 
 # resource "aws_s3_bucket_policy" "static" {
