f94eae2484153970525278a49befe4c03b54e425
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-15 22:35:22 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,74 +1,142 @@
+# # terraform {
+# #   required_providers {
+# #     aws = {
+# #       source = "hashicorp/aws"
+# #       version = "4.64.0"
+# #     }
+# #   }
+# # }
+
+# # # provider "aws" {
+# # # #     access_key = ""
+# # # #     secret_key = ""
+# # #       	region = "us-east-1"
+# # # }
+
+# # # # resource "aws_s3_bucket" "static" {
+# # # #   bucket        = "gh-js"
+# # # #   force_destroy = true
+# # # # }
+# # # module "s3_bucket" {
+# # #   source = "terraform-aws-modules/s3-bucket/aws"
+
+# # #   bucket = "gh-js"
+# # #   acl    = "private"
+
+# # #   control_object_ownership = true
+# # #   object_ownership         = "ObjectWriter"
+
+# # #   versioning = {
+# # #     enabled = true
+# # #   }
+# # # }
+# # # # resource "aws_s3_bucket_public_access_block" "static" {
+# # # #     bucket = aws_s3_bucket.static.bucket
+# # # #     block_public_acls = false
+# # # #     block_public_policy = false
+# # # #     ignore_public_acls = false
+# # # #     restrict_public_buckets = false
+# # # # }
+
+# # # # resource "aws_s3_bucket_ownership_controls" "static" {
+# # # #     bucket = aws_s3_bucket.static.bucket
+# # # #     rule {
+# # # #         object_ownership = "BucketOwnerPreferred"
+# # # #     }
+# # # # }
+# # # # resource "aws_s3_bucket_website_configuration" "static" {
+# # # #   bucket = aws_s3_bucket.static.bucket
+
+# # # #   index_document {
+# # # #     suffix = "index.html"
+# # # #   }
+
+# # # #   error_document {
+# # # #     key = "error.html"
+# # # #   }
+# # # # }
+
+# # # # resource "aws_s3_bucket_policy" "static" {
+# # # #   bucket = aws_s3_bucket.static.id
+# # # #   policy = file("s3_static_policy.json")
+# # # # }
+
+
+# # module "s3_bucket" {
+# #   source  = "terraform-aws-modules/s3-bucket/aws"
+# #   version = "3.8.2"
+
+# #   bucket        = "gh-js"
+
+# #   force_destroy = true
+
+# #   website = {
+# #     index_document = "index.html"
+# #     error_document = "error.html"
+# #   }
+
+# # }
+
+# # data "aws_iam_policy_document" "read_access" {
+# #   statement {
+# #     principals {
+# #       type        = "*"
+# #       identifiers = ["*"]
+# #     }
+# #     actions   = ["s3:GetObject"]
+# #     resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
+# #   }
+# # }
+
+# # # Need for avoid `Error putting S3 policy: AccessDenied: Access Denied`
+# # resource "time_sleep" "wait_2_seconds" {
+# #   depends_on      = [module.s3_bucket.s3_bucket_website_domain]
+# #   create_duration = "2s"
+# # }
+
+# # resource "aws_s3_bucket_policy" "read_access" {
+# #   bucket = module.s3_bucket.s3_bucket_id
+# #   policy = data.aws_iam_policy_document.read_access.json
+
+# #   depends_on = [
+# #     time_sleep.wait_2_seconds
+# #   ]
+# # }
+
 # terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.64.0"
+#   required_version = ">= 1.0.11"
+# }
+# variable "env" {
+#   description = "The name of the environment we are deploying to"
+#   type        = string
+#   default     = "test"
+# }
+# provider "aws" {
+#   access_key = "AKIA3UNIP7FP66UYIFDW"
+#   secret_key = "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"
+#   region = "us-east-1"
+#   default_tags {
+#     tags = {
+#       ManagedByTerraform = true
 #     }
 #   }
 # }
-
-# # provider "aws" {
-# # #     access_key = ""
-# # #     secret_key = ""
-# #       	region = "us-east-1"
-# # }
-
-# # # resource "aws_s3_bucket" "static" {
-# # #   bucket        = "gh-js"
-# # #   force_destroy = true
-# # # }
-# # module "s3_bucket" {
-# #   source = "terraform-aws-modules/s3-bucket/aws"
-
-# #   bucket = "gh-js"
-# #   acl    = "private"
-
-# #   control_object_ownership = true
-# #   object_ownership         = "ObjectWriter"
-
-# #   versioning = {
-# #     enabled = true
-# #   }
-# # }
-# # # resource "aws_s3_bucket_public_access_block" "static" {
-# # #     bucket = aws_s3_bucket.static.bucket
-# # #     block_public_acls = false
-# # #     block_public_policy = false
-# # #     ignore_public_acls = false
-# # #     restrict_public_buckets = false
-# # # }
-
-# # # resource "aws_s3_bucket_ownership_controls" "static" {
-# # #     bucket = aws_s3_bucket.static.bucket
-# # #     rule {
-# # #         object_ownership = "BucketOwnerPreferred"
-# # #     }
-# # # }
-# # # resource "aws_s3_bucket_website_configuration" "static" {
-# # #   bucket = aws_s3_bucket.static.bucket
-
-# # #   index_document {
-# # #     suffix = "index.html"
-# # #   }
-
-# # #   error_document {
-# # #     key = "error.html"
-# # #   }
-# # # }
-
-# # # resource "aws_s3_bucket_policy" "static" {
-# # #   bucket = aws_s3_bucket.static.id
-# # #   policy = file("s3_static_policy.json")
-# # # }
-
-
 # module "s3_bucket" {
 #   source  = "terraform-aws-modules/s3-bucket/aws"
 #   version = "3.8.2"
 
-#   bucket        = "gh-js"
+#   bucket = "gh-js"
+#   acl           = "log-delivery-write"
 
+#   # Allow deletion of non-empty bucket
 #   force_destroy = true
+
+#   attach_elb_log_delivery_policy = true # Required for ALB logs
+#   attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
+
+#   control_object_ownership = true
+#   object_ownership         = "ObjectWriter"
+#   # force_destroy = true
 
 #   website = {
 #     index_document = "index.html"
@@ -77,30 +145,47 @@
 
 # }
 
-# data "aws_iam_policy_document" "read_access" {
-#   statement {
-#     principals {
-#       type        = "*"
-#       identifiers = ["*"]
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "4.44.0"
 #     }
-#     actions   = ["s3:GetObject"]
-#     resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
 #   }
 # }
 
-# # Need for avoid `Error putting S3 policy: AccessDenied: Access Denied`
-# resource "time_sleep" "wait_2_seconds" {
-#   depends_on      = [module.s3_bucket.s3_bucket_website_domain]
-#   create_duration = "2s"
+# provider "aws" {
+#     	access_key = "AKIA3UNIP7FP66UYIFDW"
+# 	    secret_key = "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"
+#       region = "us-east-1"
 # }
 
-# resource "aws_s3_bucket_policy" "read_access" {
-#   bucket = module.s3_bucket.s3_bucket_id
-#   policy = data.aws_iam_policy_document.read_access.json
+# resource "aws_s3_bucket" "static" {
+#   bucket        = "gh-js"
+#   force_destroy = true
+# }
 
-#   depends_on = [
-#     time_sleep.wait_2_seconds
-#   ]
+# resource "aws_s3_bucket_acl" "static" {
+#   bucket = aws_s3_bucket.static.id
+#   acl    = "public-read"
+# }
+
+# resource "aws_s3_bucket_website_configuration" "static" {
+#   bucket = aws_s3_bucket.static.bucket
+
+#   index_document {
+#     suffix = "index.html"
+#   }
+
+#   error_document {
+#     key = "error.html"
+#   }
+# }
+
+
+# resource "aws_s3_bucket_policy" "static" {
+#   bucket = aws_s3_bucket.static.id
+#   policy = file("s3_static_policy.json")
 # }
 
 terraform {
@@ -126,21 +211,49 @@ module "s3_bucket" {
   version = "3.8.2"
 
   bucket = "gh-js"
-  acl           = "log-delivery-write"
+  acl           = "public-read-write"
 
   # Allow deletion of non-empty bucket
   force_destroy = true
 
-  attach_elb_log_delivery_policy = true # Required for ALB logs
-  attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
-
+  # attach_elb_log_delivery_policy = true # Required for ALB logs
+  # attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
+  # attach_public_policy = true
+  # block_public_policy = false
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
   # force_destroy = true
-
+  policy = data.aws_iam_policy_document.s3_read_permissions.json
+  # jsonencode({
+  #   "Version": "2012-10-17",
+  #   "Statement": [
+  #       {
+  #           "Principal": "*",
+  #           "Action": "s3:GetObject",
+  #           "Resource": [
+  #               "arn:aws:s3:::gh-js/*"
+  #           ],
+  #           "Effect": "Allow"
+  #       }
+  #   ]
+  # })
   website = {
     index_document = "index.html"
     error_document = "error.html"
   }
+}
+data "aws_iam_policy_document" "s3_read_permissions" {
+  statement {
+    effect = "Allow"
 
+    actions = [
+      "s3:GetObject",
+      # "s3:GetObjectAcl",
+      # "s3:ListBucket",
+    ]
+
+    resources = ["arn:aws:s3:::gh-js/*",
+ 
+    ]
+  }
 }
