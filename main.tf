df8826089d420ea2c1e39cbd6185f3a5b4f0c307
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-16 15:23:11 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,68 @@
 terraform {
-  required_version = ">= 1.0.11"
-}
-variable "env" {
-  description = "The name of the environment we are deploying to"
-  type        = string
-  default     = "test"
-}
-provider "aws" {
-#   access_key = ""
-#   secret_key = ""
-  region = "us-east-1"
-  default_tags {
-    tags = {
-      ManagedByTerraform = true
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.44.0"
     }
   }
 }
-module "s3_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.8.2"
 
-  bucket = "gh-js"
-  acl           = "null"
-
-  # Allow deletion of non-empty bucket
-  force_destroy = true
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-  # attach_elb_log_delivery_policy = true # Required for ALB logs
-  # attach_lb_log_delivery_policy  = true # Required for ALB/NLB logs
-#   attach_public_policy = true
-  block_public_policy = false
-  # force_destroy = true
-#   attach_policy = true
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": [
-                "arn:aws:s3:::gh-js/*"
-            ],
-            "Effect": "Allow"
-        }
-    ]
+provider "aws" {
+    region = "us-east-1"
 }
-EOF
-website = {
-    index_document = "index.html"
-    error_document = "error.html"
+# resource "aws_s3_bucket" "test" {
+#   bucket        = "test_bucket"
+#   force_destroy = true
+# }
+resource "aws_s3_bucket" "static" {
+  bucket        = "gh-js"
+  force_destroy = true
+}
+# resource "aws_s3_bucket_policy" "static" {
+#   bucket = aws_s3_bucket.static.id
+#   policy = <<EOF
+#   {
+#    "Version":"2012-10-17",
+#    "Id":"MYBUCKETPOLICY",
+#    "Statement":[
+#       {
+#          "Effect":"Allow",
+#          "Principal":"*",
+#          "Action":"s3:*",
+#          "Resource":"arn:aws:s3:::gh-js/*"
+#       }
+#    ]
+# }
+#   EOF
+# }
+# resource "aws_s3_bucket" "static" {
+#   bucket        = "gh-js"
+#   force_destroy = true
+#   # provisioner "local-exec" {
+#   #   interpreter = ["PowerShell", "-Command"]
+#   #   command = "start-sleep 300"
+#   # }
+# }
+# 
+resource "aws_s3_bucket_website_configuration" "static" {
+  bucket = aws_s3_bucket.static.bucket
+
+  index_document {
+    suffix = "index.html"
   }
+
+  error_document {
+    key = "error.html"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "static" {
+  bucket = aws_s3_bucket.static.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+}
+
+resource "aws_s3_bucket_policy" "static" {
+  bucket = aws_s3_bucket.static.id
+  policy = file("s3_static_policy.json")
 }
