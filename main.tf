ddbebee40649b9ca82dfe2d49eb0eff8579ca24c
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-17 10:18:27 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_ownership_controls" "static" {
 
   rule {
     object_ownership = "ObjectWriter"
-    acls_disabled = true
+#     acls_disabled = true
   }
 }
 # resource "aws_s3_bucket_acl" "static" {
