6afdebb492e978bbd3d44d0dfa884c5de940ee22
nguyeny1994 <86542825+nguyeny1994@users.noreply.github.com>
2023-05-17 10:38:14 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,40 @@ resource "aws_s3_bucket" "static" {
   bucket        = "gh-js"
   force_destroy = true
 #   acl = "public-read"
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
 }
+resource "aws_s3_bucket_public_access_block" "static" {
+  bucket = aws_s3_bucket.static.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+}
+
+resource "aws_s3_bucket_policy" "static" {
+  bucket = aws_s3_bucket.static.id
+  policy = file("s3_static_policy.json")
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "static" {
+  bucket = aws_s3_bucket.static.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+#     acls_disabled = true
+  }
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+}
+
 
 resource "aws_s3_bucket_website_configuration" "static" {
   bucket = aws_s3_bucket.static.bucket
@@ -27,28 +60,16 @@ resource "aws_s3_bucket_website_configuration" "static" {
   error_document {
     key = "error.html"
   }
-}
-resource "aws_s3_bucket_ownership_controls" "static" {
-  bucket = aws_s3_bucket.static.id
-
-  rule {
-    object_ownership = "ObjectWriter"
-#     acls_disabled = true
+  provisioner "local-exec" {
+    command = "sleep 10"
   }
 }
+
 # resource "aws_s3_bucket_acl" "static" {
 #   bucket = aws_s3_bucket.static.id
 #   acl    = "public-read"
 # }
 
-resource "aws_s3_bucket_public_access_block" "static" {
-  bucket = aws_s3_bucket.static.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-}
 
-resource "aws_s3_bucket_policy" "static" {
-  bucket = aws_s3_bucket.static.id
-  policy = file("s3_static_policy.json")
-}
+
